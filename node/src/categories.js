865b40b9d808e1c68428a382bdf1bb0d723916fa
Tyler Miller <tylermillerhome@gmail.com>
2016-10-08 13:32:11 -0700

    Node side almost done, need to know response info

---
+++ node/src/categories.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "earth science": "earth science",
+  "public saftey": "public saftey",
+  "government": "government",
+  "social services": "social services",
+  "health": "health",
+  "transportation": "transportation",
+  "city government": "city government",
+  "education": "education",
+  "loan and credit administration": "loan and credit administration",
+  "energy and environment": "energy and environment"
+};
