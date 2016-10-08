c0e87e7d9307ee98899319566f8020b36973bca6
Tyler Miller <tylermillerhome@gmail.com>
2016-10-08 09:55:09 -0700

    Basic alexa set

---
+++ node/src/index.js
@@ -0,0 +1,23 @@
+Fact.prototype.intentHandlers = {
+    "GetNewFactIntent": function (intent, session, response) {
+        handleNewFactRequest(response);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        response.ask("Ask me what what dataset you should use.", "What can I help you with?");
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    }
+};
+
+function handleNewFactRequest(res) {
+
+}
